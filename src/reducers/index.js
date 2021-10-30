eb5d889dc97acb1889b19630b55b5947b6372f6c
Shravan <shravan.akku@gmail.com>
2021-10-30 11:49:03 +0530

    feat: created reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        { title: 'Title 1', duration: '4:05' },
+        { title: 'Title 2', duration: '2:05' },
+        { title: 'Title 3', duration: '3:05' },
+        { title: 'Title 4', duration: '1:05' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectSong;
+};
